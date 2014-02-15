e06354eae82c84604885904b39c979b14fb12f33
Chang,Liang <bigbrightf@gmail.com>
2014-02-16 00:52:33 +0800

    update 20140216 Matchismo: match cards with the same rank ignore difference on suits

---
+++ 20140209.Matchismo/20140209.Matchismo/Model/CardMachingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMachingGame.h
+//  20140209.Matchismo
+//
+//  Created by Chang, Liang on 14-2-12.
+//  Copyright (c) 2014年 Chang, Liang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMachingGame : NSObject
+
+@end
---
+++ 20140209.Matchismo/20140209.Matchismo/Model/CardMachingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMachingGame.m
+//  20140209.Matchismo
+//
+//  Created by Chang, Liang on 14-2-12.
+//  Copyright (c) 2014年 Chang, Liang. All rights reserved.
+//
+
+#import "CardMachingGame.h"
+
+@implementation CardMachingGame
+
+@end
